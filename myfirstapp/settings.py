422bb26ed0bc05fa438034290a6a59ef02843eb5
Sanan <asannan822@gmail.com>
2021-07-15 20:28:38 +0530

    Remove key

--- myfirstapp/settings.py
+++ myfirstapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9**$e(*1@o75d2b5g144mv$naug#h=n&8y^+n7*b2luv47#3-k'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
